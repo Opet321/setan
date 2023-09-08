36bb94db0fd8ed403311fced7dfaa2c40f4128e0
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-09-08 12:59:46 +0700

    Update broadcast.py

--- modules/broadcast.py
+++ modules/broadcast.py
@@ -50,7 +50,7 @@ async def gcast(event):
     async for x in event.client.iter_dialogs():
         if x.is_group:
             chat = x.id
-            if chat not in chat_blacklist and chat not in NOSPAM_CHAT:
+            if chat not in chat_blacklist and chat not in NOSPAM_CHAT in not None:
                 try:
                     await event.client.send_message(chat, msg)
                     done += 1
