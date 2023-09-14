7fe59a8367d0485696486cde92f69505de86ecc0
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-09-14 23:14:08 +0700

    Update tiktok.py

--- modules/tiktok.py
+++ modules/tiktok.py
@@ -29,7 +29,7 @@ except ImportError:
 
 from . import *
 
-@ayra_cmd(pattern="[Tt][k](?: |$)(.*),")
+@ayra_cmd(pattern="[Tt][k](?: |$)(.*)",)
 async def _(event):
     if xxnx := event.pattern_match.group(1):
         link = xxnx
